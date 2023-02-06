9db9885a3ecff13fe3396faf5b6e25df2d9ec9d4
Sanket Patel <sanketpatel241@gmail.com>
2023-02-06 20:58:23 +0000

    first commit

---
+++ First.java
@@ -0,0 +1,10 @@
+class First{
+	public static void main(String[] args){
+
+	String companyName="Albany Beck";
+	System.out.println("Hey Welcome to Java Programming");
+System.out.println("Hey Welcome to"+ companyName + " Training");
+
+
+}
+}
